32fdac9968b6714cf227f03fdcf5b4d6fc441177
andrewinkler <andre.winkler@web.de>
2019-11-04 06:45:00 +0100

    Get current date and time with button click.

--- src/main/resources/static/Client.js
+++ src/main/resources/static/Client.js
@@ -2,14 +2,63 @@ import RestCaller from './RestCaller.js';
 
 export default class Client {
     constructor() {
+        this.$dateTime = null;
         this.restCaller = new RestCaller();
         this.app = document.getElementById('app');
-        this.restCaller.dates().then(dates => this.render(dates));
+        this.getCurrentDateTime();
     }
 
-    render(dates) {
+    getCurrentDateTime() {
+        this.restCaller.dates().then(dates => this.storeDate(dates));
+    }
+
+    storeDate(dates) {
+        this.$dateTime = dates;
         console.log(dates);
     }
 }
 
-let client = new Client();
+const template = document.createElement('template');
+template.innerHTML = `<button>Get Time</button><br/><b>Uhrzeit:</b><div id="dateTime"></div>`;
+
+class DateTimeElement extends HTMLElement {
+    constructor() {
+        // always call super() first
+        super(); 
+        console.log('constructed!');
+
+        this.client = new Client();
+
+        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
+        this.$dateTime = this._shadowRoot.querySelector('div');
+
+        this.$getTimeButton = this._shadowRoot.querySelector('button');
+        this.$getTimeButton.addEventListener('click', (e) => {
+            this.client.getCurrentDateTime();
+        });
+    }
+
+    render(dateTime) {
+        this.$dateTime.innerHTML = dateTime;
+    }
+
+    connectedCallback() {
+        console.log('connected!');
+    }
+
+    disconnectedCallback() {
+        console.log('disconnected!');
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        console.log(`Attribute: ${name} changed!`);
+    }
+
+    adoptedCallback() {
+        console.log('adopted!');
+    }
+}
+
+window.customElements.define('date-time', DateTimeElement);
+
