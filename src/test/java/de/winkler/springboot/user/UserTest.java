8bc3508a901c1dde0c0e1ba43a78710b639bdfb8
andrewinkler <andre.winkler@web.de>
2020-03-08 14:48:13 +0100

    Take care of your dependencies.

--- src/test/java/de/winkler/springboot/user/UserTest.java
+++ src/test/java/de/winkler/springboot/user/UserTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class UserTest {
 
     @Autowired
