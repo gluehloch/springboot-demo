e3e32dac0b497cb5b5f797e369da135fd33eb746
Andre Winkler <gluehloch@googlemail.com>
2023-05-23 11:47:16 +0200

    Trying to code.

--- src/test/java/de/winkler/springboot/security/LoginServiceTest.java
+++ src/test/java/de/winkler/springboot/security/LoginServiceTest.java
@@ -7,11 +7,9 @@ import de.winkler.springboot.user.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import jakarta.transaction.Transactional;
 
--- src/test/java/de/winkler/springboot/user/UserServiceTest.java
+++ src/test/java/de/winkler/springboot/user/UserServiceTest.java
@@ -3,10 +3,8 @@ package de.winkler.springboot.user;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import jakarta.transaction.Transactional;
 
