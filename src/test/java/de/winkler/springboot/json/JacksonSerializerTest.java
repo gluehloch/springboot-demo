c1a0ab8a6a173170b50778a85a79bdf663d623d8
gluehloch <gluehloch@googlemail.com>
2023-12-21 12:46:15 +0100

    Java map to JSON.

--- src/test/java/de/winkler/springboot/json/JacksonSerializerTest.java
+++ src/test/java/de/winkler/springboot/json/JacksonSerializerTest.java
@@ -2,13 +2,17 @@ package de.winkler.springboot.json;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.winkler.springboot.user.Nickname;
 
-class JacksonSerialiizerTest {
+class JacksonSerializerTest {
     
     @Test
     void serializer() throws Exception {
@@ -19,4 +23,14 @@ class JacksonSerialiizerTest {
         objectMapper.readTree(jsonString);
     }
 
+    @Test
+    void mapToJson() throws JsonProcessingException {
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("key1","value1");
+        payload.put("key2","value2");
+        payload.put("key3", 1001L);
+
+        String json = new ObjectMapper().writeValueAsString(payload);
+        assertThat(json).isEqualTo("{\"key1\":\"value1\",\"key2\":\"value2\",\"key3\":1001}");
+    }
 }
